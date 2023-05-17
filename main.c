b90d71f5e748e747dfdf688cfd733fb96ca78057
ghaidaareda <dodo.sa7afy@gmail.com>
2023-05-17 14:46:29 -0700

    main.c division

--- main.c
+++ main.c
@@ -1,83 +1,77 @@
 #include "main.h"
 
-/**
- * main - the main function for our simple shell
- *
- * Return: 0(sucess) or error msg(error)
- * @ac:arguments count
- * @argv:arguments variables
- */
+void read_input(char **lineptr, size_t *n) {
+    ssize_t char_num = getline(lineptr, n, stdin);
+    if (char_num == -1) {
+        perror("Exit\n");
+        exit(EXIT_FAILURE);
+    }
+    (*lineptr)[strcspn(*lineptr, "\n")] = '\0';
+}
+
+char **parse_input(char *line, const char *delim, int *token_num) {
+    char *token;
+    int i;
+    char **argv;
+    *token_num = 0;
+    token = strtok(line, delim);
+    while (token != NULL) {
+        (*token_num)++;
+        token = strtok(NULL, delim);
+    }
+    (*token_num)++;
+    argv = malloc(sizeof(char*) * (*token_num));
+    token = strtok(line, delim);
+    for (i = 0; token != NULL; i++) {
+        argv[i] = strdup(token);
+        token = strtok(NULL, delim);
+    }
+    argv[i] = NULL;
+    return argv;
+}
+
+void execute_command(char **argv) {
+        pid_t child;
+    if (is_builtin(argv)) {
+        exit_shell();
+    }
+    child = fork();
+    if (child == -1) {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    } else if (child == 0) {
+        execute(argv);
+        printf("%s: No such file or directory\n", argv[0]);
+        exit(EXIT_FAILURE);
+    } else {
+        wait(NULL);
+    }
+}
+
+void cleanup(char **argv, char *lineptr, char *lineptr_copy) {
+    free(argv);
+    free(lineptr_copy);
+    free(lineptr);
+}
 
 int main(int ac, char **argv)
 {
-	char *shell_prompt = "our_shell$ ";
-	char *lineptr = NULL; char *lineptr_copy = NULL;
-	size_t n = 0;
-	ssize_t char_num;
-	const char *delim = " \n";
-	int token_num = 0;
-	char *token;
-	int i;
-	pid_t child;
-	(void) ac;
-	while (5)
-	{
-		printf("%s", shell_prompt);
-		char_num = getline(&lineptr, &n, stdin);
-		 if (char_num == -1)
-                {
-		printf("Exit\n");
-		free(lineptr);
-		return (-1); EXIT_FAILURE;
-                }
-		 lineptr[strcspn(lineptr, "\n")] = '\0';
-		lineptr_copy = malloc(sizeof(char) * char_num);
-		if (lineptr_copy == NULL)
-		{
-			printf("memory allocation error\n");
-				return (-1);
-		}
-		strcpy(lineptr_copy, lineptr);
-		token = strtok(lineptr, delim);
-		while (token != NULL)
-		{
-			token_num++;
-			token = strtok(NULL, delim);
-		}
-		token_num++;
-		argv = malloc(sizeof(char*) * token_num);
-		token = strtok(lineptr_copy, delim);
-		for (i = 0; token != NULL; i++)
-		{
-			argv[i] = malloc(sizeof(char) * strlen(token));
-			strcpy(argv[i], token);
-			token = strtok(NULL, delim);
-		}
-		argv[i] = NULL;
-
-                        if (is_builtin(argv))
-                        {
-                        exit_shell();
-                        }
-		child = fork();
-                if (child == -1)
-                {
-                        free(lineptr), exit(EXIT_FAILURE);
-                }
-                if (child == 0)
-                {
-		execute(argv); 
-		printf("%s: No such file or directory\n", argv[0]);
-                }
-                else
-                {
-                        wait(NULL);
-           }
-		getenv("PATH");
-	}
-		free(argv);
-	
-                free(lineptr_copy);
-                free(lineptr);
-	return (0);
+    char *shell_prompt = "our_shell$ ";
+    char *lineptr = NULL, *lineptr_copy = NULL;
+    size_t n = 0;
+    const char *delim = " \n";
+    int token_num;
+    int interactive_mode = isatty(STDIN_FILENO);
+    (void)ac;
+    if (interactive_mode) {
+        while (1) {
+            printf("%s", shell_prompt);
+            read_input(&lineptr, &n);
+            lineptr_copy = strdup(lineptr);
+            argv = parse_input(lineptr, delim, &token_num);
+            execute_command(argv);
+        }
+    }
+    cleanup(argv, lineptr, lineptr_copy);
+    return 0;
 }
