e7d92104fa921c7b4b54fd31985ec8a93a0f1686
ghaidaareda <dodo.sa7afy@gmail.com>
2023-05-18 12:03:32 -0700

    update

--- exit.c
+++ exit.c
@@ -1,7 +1,12 @@
 #include "main.h"
+
 void exit_shell()
 {
     printf("Exiting shell...\n");
     fflush(stdout);
     exit(0);
+}
+
+int is_builtin(char **argv){
+    return strcmp(argv[0], "exit") == 0;
 }
