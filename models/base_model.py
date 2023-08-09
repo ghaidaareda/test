12a3a3a34bbe84db912512ba2818ce748c232796
ghaidaareda <dodo.sa7afy@gmail.com>
2023-08-09 12:06:12 -0700

    dict

--- models/base_model.py
+++ models/base_model.py
@@ -8,12 +8,22 @@ from uuid import uuid4
 from datetime import datetime
 class BaseModel:
     """ main class of prject """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """ init method """
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-
+        if kwargs:
+            for key in kwargs:
+                if key == '__class__':
+                    continue
+                elif key == "created_at":
+                    self.__dict__["created_at"] = datetime.fromisoformat(kwargs["created_at"])
+                elif key == "updated_at":
+                    self.__dict__["updated_at"] = datetime.fromisoformat(kwargs["updated_at"])
+                else:
+                    self.__dict__[key] = kwargs[key]
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
     def __str__(self):
         """ string representation of class """
         return f"[{self.__class__.__name__}]({self.id}) {self.__dict__}"
@@ -24,7 +34,7 @@ class BaseModel:
 
     def to_dict(self):
         """ returns a dictionary containing all keys/values """
-        my_dict = {key: value for key, value in self.__dict__.items()}
+        my_dict = self.__dict__.copy()
         my_dict["__class__"] = self.__class__.__name__
         my_dict["created_at"] = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
         my_dict["updated_at"] = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
