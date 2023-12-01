d667a6ca53fbbb6a06b72ae7a8fcab878c889dff
ghaidaareda <dodo.sa7afy@gmail.com>
2023-12-01 03:10:25 +0200

    del

--- mylog/project/mommyblog/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-#from . import views
-from .views import HomeView, ArticleView
-urlpatterns = [
-    #path("", views.home, name="home"),
-    path('', HomeView.as_view(), name="home"),
-    path('article/<int:pk>', ArticleView.as_view(), name="article")
-]
